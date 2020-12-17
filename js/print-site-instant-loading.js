70a9001537d76cb3f0c97f2c5c7d92efc30bd370
Tim Vink <vinktim@gmail.com>
2020-12-17 20:37:56 +0100

    Deployed 23fbfd8 with MkDocs version: 1.1.2

--- js/print-site-instant-loading.js
+++ js/print-site-instant-loading.js
@@ -1,9 +1,6 @@
 
          // Subscribe functions for compatibility 
          // with mkdocs-material's instant loading feature
-         
-         body = document.getElementsByTagName('body')[0];
-         mkdocs_material_site_color_theme = body.getAttribute('data-md-color-scheme');
                     
          if (
             typeof app !== "undefined" && 
@@ -11,13 +8,8 @@
             ) {
             app.document$.subscribe(function() {
                 if ( document.querySelector("#print-site-page") !== null ) {
-                    change_material_theme('default');generate_toc();
-                } else {
-                    // Make sure to change the color theme back!
-                    if ( mkdocs_material_site_color_theme !== null ) {
-                        change_material_theme(mkdocs_material_site_color_theme);   
-                    }
-                }
+                    generate_toc();
+            }
             })
          }
         
